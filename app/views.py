34b3a31a093f98dbab8c6e8e94cd98572e2bb1bd
mishael254 <mishaelmishantoz@gmail.com>
2020-07-20 10:42:52 +0300

    create a new dynamic route for news

--- app/views.py
+++ app/views.py
@@ -7,3 +7,11 @@ def index():
     a view route page function that returns index page and its data
     '''
     return render_template('index.html')
+
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    '''
+    a news article function that returns news article details and its data
+    '''
+    return render_template('news.html',id = news_id)
