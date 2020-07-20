a5a656f49f0c361abeee230f8be2f360f34cf60e
mishael254 <mishaelmishantoz@gmail.com>
2020-07-20 10:57:10 +0300

    configure the app and import the dev config class from config.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
+#initializing the app
 app = Flask(__name__)
 
+
+#configuring the app
+app.config.from_object(DevConfig)
 from app import views
