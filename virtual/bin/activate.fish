481536655e8fdde7494232e1826a283dac6119df
mishael254 <mishaelmishantoz@gmail.com>
2020-07-21 12:34:11 +0300

    replaced the virtual files environment together with bootstrap and flask files

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mishael/Desktop/news-app/virtual"
+set -gx VIRTUAL_ENV "/home/mishael/Desktop/news-master/news-master/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
