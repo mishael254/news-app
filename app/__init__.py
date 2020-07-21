40f950f27e9d62aad97cd50bd50c6d7b2458a2a1
mishael254 <mishaelmishantoz@gmail.com>
2020-07-21 12:23:05 +0300

    import bootstrap from module flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,31 @@
 from flask import Flask
-from .config import DevConfig
-
-#initializing the app
-app = Flask(__name__,instance_relative_config=True)
+from flask_bootstrap import Bootstrap
+from config import config_options
 
 
-#configuring the app
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-from app import views
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting configuration
+    from .request import configure_request
+    configure_request(app)
+    
+
+
+
+    return app
